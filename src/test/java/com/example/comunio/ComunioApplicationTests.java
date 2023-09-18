49d60f57722385e1aacf921dbff602c3d8d61b5e
mcpape <mpape234@gmail.com>
2023-09-18 10:08:36 +0200

    init

---
+++ src/test/java/com/example/comunio/ComunioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comunio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
